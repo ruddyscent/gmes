113cf665183989abd1d39c28c82f5bdba9539b56
Kyungwon Chun <ruddyscent@gmail.com>
2026-08-21 12:32:43 +0900

    docs: standardize GPL license metadata
    Clarify that GMES is distributed under GPL-3.0-or-later and rename the license file to the conventional LICENSE name. Align the README, package metadata, and source notice while replacing the ambiguous GNU Library GPL wording.

--- gmes/__init__.py
+++ gmes/__init__.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 ##    gmes - GIST Maxwell's Equations Solver
 ##    Copyright (C) 2007-2012  Kyungwon Chun
 ##
-##    This library is free software; you can redistribute it and/or
-##    modify it under the terms of the GNU Library General Public
+##    This program is free software; you can redistribute it and/or
+##    modify it under the terms of the GNU General Public
 ##    License as published by the Free Software Foundation; either
 ##    version 3 of the License, or (at your option) any later version.
 ##
-##    This library is distributed in the hope that it will be useful,
+##    This program is distributed in the hope that it will be useful,
 ##    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-##    Library General Public License for more details.
+##    General Public License for more details.
 ##
-##    You should have received a copy of the GNU Library General Public
-##    License along with this library; if not, write to the Free
-##    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+##    You should have received a copy of the GNU General Public
+##    License along with this program. If not, see
+##    <https://www.gnu.org/licenses/>.
 ##
 ##    Kyungwon Chun
 ##    kwchun@gist.ac.kr
